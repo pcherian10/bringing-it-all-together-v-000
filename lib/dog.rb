04c2fba4280bebe5fc8b7c79b08f1ace26e50d9e
Paul <pcherian10@gmail.com>
2018-06-09 23:43:35 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -38,6 +38,7 @@ class Dog
         INSERT INTO dogs (name, breed)
         VALUES (?,?)
       SQL
+      binding.pry
       self.new_from_db(DB[:conn].execute(sql, self.name, self.breed))
       #@id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
   end
